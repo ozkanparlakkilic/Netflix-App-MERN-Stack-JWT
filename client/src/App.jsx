e6f632dd1fd24358b76a3af9589d6b983b124fab
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-06-27 23:56:06 +0300

    Add user context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,33 +9,37 @@ import Login from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
 import { Watch } from "./pages/watch/Watch";
 
+import { UserProvider } from "./providers/UserProvider";
+
 const App = () => {
-  const user = true;
+  const user = false;
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Redirect to="register" />}
-        </Route>
-        <Route path="/register">
-          {!user ? <Register /> : <Redirect to="/" />}
-        </Route>
-        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
-        {user && (
-          <>
-            <Route path="/movies">
-              <Home type="movies" />
-            </Route>
-            <Route path="/series">
-              <Home type="series" />
-            </Route>
-            <Route path="/watch">
-              <Watch />
-            </Route>
-          </>
-        )}
-      </Switch>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {user ? <Home /> : <Redirect to="register" />}
+          </Route>
+          <Route path="/register">
+            {!user ? <Register /> : <Redirect to="/" />}
+          </Route>
+          <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+          {user && (
+            <>
+              <Route path="/movies">
+                <Home type="movies" />
+              </Route>
+              <Route path="/series">
+                <Home type="series" />
+              </Route>
+              <Route path="/watch">
+                <Watch />
+              </Route>
+            </>
+          )}
+        </Switch>
+      </Router>
+    </UserProvider>
   );
 };
 
